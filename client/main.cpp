debfca40341eb01dfe5cad099518e28114f68772
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-10-04 13:15:17 +0900

    プロジェクト ファイルを追加します。

---
+++ client/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+
+#pragma comment(lib, "WSock32.lib")
+
+#include <array>
+
+#include <string>
+
+int main() {
+	try {
+		WSADATA wsaData;
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+			
+			throw std::runtime_error("WinSock initialize failed");
+		}
+
+		std::cout << "ポート番号の入力 : ";
+
+		uint32_t uport = 0;
+		std::cin >> uport;
+		
+		// ソケット作成
+		SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+		if (s == INVALID_SOCKET) {
+			throw std::runtime_error("Create socket failed");
+		}
+
+		std::string szServer;
+		szServer.resize(256);
+		szServer = "192.168.0.60";
+
+		HOSTENT* lpHost = nullptr;
+		lpHost = gethostbyname(szServer.data());
+
+
+		SOCKADDR_IN saddr;
+		memset(&saddr, 0, sizeof(saddr));
+		saddr.sin_family = lpHost->h_addrtype;
+		saddr.sin_port = htons(uport);
+		saddr.sin_addr.s_addr = *((u_long*)lpHost->h_addr);
+
+		if (connect(s, reinterpret_cast<SOCKADDR*>(&saddr), sizeof(saddr)) == SOCKET_ERROR) {
+			closesocket(s);
+			throw std::runtime_error("server conectted faile");
+		}
+		else {
+			std::cout << "接続成功" << std::endl;
+		}
+
+		while (1) {
+			int32_t nRcv;
+			std::string szBuf;
+			szBuf.clear();
+			szBuf.resize(1024);
+
+			std::cout << "送信 -->";
+			std::cin >> szBuf;
+
+			send(s, szBuf.data(), static_cast<int32_t>(szBuf.size()), 0);
+			if (szBuf == "end") {
+				break;
+			}
+
+			szBuf.clear();
+			szBuf.resize(1024);
+
+			std::cout << "受信待機中" << std::endl;
+
+			nRcv = recv(s, szBuf.data(), static_cast<int32_t>(szBuf.size()) - 1, 0);
+			szBuf[nRcv] = '\0';
+			std::cout << "受信 -->" << szBuf << std::endl;
+			if (szBuf == "end") {
+				break; // なぜか通らない
+			}
+		}
+
+		closesocket(s);
+
+		WSACleanup();
+	}
+	catch (const std::exception& err) {
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
---
+++ server/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+
+#pragma comment(lib, "WSock32.lib")
+
+#include <array>
+
+#include <string>
+
+int main() {
+	try {
+		WSADATA wsaData;
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+			
+			throw std::runtime_error("WinSock initialize failed");
+		}
+
+		std::cout << "ポート番号の入力 : ";
+
+		uint32_t uport = 0;
+		std::cin >> uport;
+		
+		// ソケット作成
+		SOCKET conectSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (conectSocket == INVALID_SOCKET) {
+			throw std::runtime_error("Create socket failed");
+		}
+
+
+		SOCKADDR_IN saddr;
+		memset(&saddr, 0, sizeof(saddr));
+		saddr.sin_family = AF_INET;
+		saddr.sin_port = htons(uport);
+		saddr.sin_addr.s_addr = INADDR_ANY;
+
+		int32_t bindNum = bind(conectSocket, reinterpret_cast<sockaddr*>(&saddr), sizeof(saddr));
+		if (bindNum == SOCKET_ERROR) {
+			throw std::runtime_error("bind socket failed");
+		}
+
+		int32_t listenNum = listen(conectSocket, 0);
+		if (listenNum == SOCKET_ERROR) {
+			throw std::runtime_error("listen failed");
+		}
+
+		SOCKADDR_IN clientSaddr;
+		int32_t size = static_cast<int32_t>(sizeof(clientSaddr));
+
+		std::cout << "accept待機中" << std::endl;
+		SOCKET s = accept(conectSocket, reinterpret_cast<sockaddr*>(&clientSaddr), &size);
+		if (s == INVALID_SOCKET) {
+			throw std::runtime_error("accept failed");
+		}
+
+		std::cout << "client接続中" << std::endl;
+
+		closesocket(conectSocket);
+
+		while (1) {
+			int32_t nRcv;
+			std::string szBuf;
+			szBuf.clear();
+			szBuf.resize(1024);
+
+			std::cout << "受信待機中" << std::endl;
+
+			nRcv = recv(s, szBuf.data(), static_cast<int32_t>(szBuf.size()) - 1, 0);
+			szBuf[nRcv] = '\0';
+			std::cout << "受信 -->" << szBuf << std::endl;
+			if (szBuf == "end") {
+				break; // なぜか通らない
+			}
+
+			szBuf.clear();
+			szBuf.resize(1024);
+
+			std::cout << "送信 -->";
+			std::cin >> szBuf;
+
+			send(s, szBuf.data(), static_cast<int32_t>(szBuf.size()), 0);
+			if (szBuf == "end") {
+				break;
+			}
+		}
+
+		closesocket(s);
+
+		WSACleanup();
+	}
+	catch (const std::exception& err) {
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
